668a65f27aa3bf777f854c542d8078bdf0e651b0
MireiaBS <barrufetsoler.mireia@gmail.com>
2022-07-05 09:18:37 +0200

    Inici

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [starships, setStarships] = useState([])
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/starships/')
+      .then(elements => {
+        /*elements.map((element) => console.log(element.data.results))*/
+      })
+      }, []) 
+      
+
+    console.log(starships)
+
+    return (
+      <div>hi</div>)
+
 }
 
 export default App;
+
+
+/*- Nom de nau.
+
+- Model 
+*/
